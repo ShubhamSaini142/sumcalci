dac7ebe3610f38353bc848f4a8e23f718fd04df5
Shubham <shubham.sms142@gmail.com>
2024-08-23 16:51:14 +0530

    Project Setup with added ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [num1 , setNum1] = useState()
+  const [num2 , setNum2] = useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+     <h1 className='center'>Sum Calculator</h1>
+      <form>
+      <div>
+        <label>Number 1</label>
+        <input type='text' placeholder='Enter the number 1' value={num1} onChange={(e)=> setNum1(e.target.value)}></input>
+
+      </div>
+      <div>
+        <label>Number 2</label>
+        <input type='text' placeholder='Enter the number 2' value={num2} onChange={(e)=> setNum2(e.target.value)}></input>
+      </div>
+      </form>
+      <div>
+        
+      </div>
+
+    </div>
     </div>
   );
 }
