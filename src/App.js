b446704d01deda7f23561733fed9f3a1ef85e938
Shubham <shubham.sms142@gmail.com>
2024-08-23 17:06:05 +0530

    sum calculator completed

--- src/App.js
+++ src/App.js
@@ -5,27 +5,37 @@ function App() {
 
   const [num1 , setNum1] = useState()
   const [num2 , setNum2] = useState()
+  const [add, setAdd] = useState('')
+
+  const calcsum = (event) => {
+    event.preventDefault() 
+    setAdd(parseInt(num1)+parseInt(num2))
+  }
   return (
-    <div className="App">
+      <div className='app'>
     <div className='container'>
      <h1 className='center'>Sum Calculator</h1>
-      <form>
+      <form onSubmit={calcsum}>
       <div>
         <label>Number 1</label>
-        <input type='text' placeholder='Enter the number 1' value={num1} onChange={(e)=> setNum1(e.target.value)}></input>
+        <input type='number' placeholder='Enter the number 1' value={num1} onChange={(e)=> setNum1(e.target.value)}></input>
 
       </div>
       <div>
         <label>Number 2</label>
-        <input type='text' placeholder='Enter the number 2' value={num2} onChange={(e)=> setNum2(e.target.value)}></input>
+        <input type='number' placeholder='Enter the number 2' value={num2} onChange={(e)=> setNum2(e.target.value)}></input>
+      </div>
+      <div>
+        <button className='btn'  type='submit'>Submit</button>
       </div>
       </form>
       <div>
-        
+        <h2>Your Sum is: {add}</h2>
       </div>
 
     </div>
     </div>
+  
   );
 }
 
